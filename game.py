602a4e457d53fc7b968d767bc9dc112fe1735cbe
jourdanbua7275 <84427943+jourdanbua7275@users.noreply.github.com>
2021-05-25 20:42:57 -0400

    Or functionality

--- game.py
+++ game.py
@@ -5,4 +5,15 @@ print("Rock, Paper, Scissors, Shoot!")
 
 user_choice = input("Please Choose One Of 'Rock', 'Paper', 'Scissors': ")
 
-print("USER CHOICE: ",user_choice)
+
+if (user_choice == "Rock") or (user_choice == "Paper") or (user_choice == "Scissors"):
+    print("Valid. Keep Going.")
+
+else:
+    print("OOPS, Invalid Input. Please try again.")
+    EXIT()
+
+
+
+
+print("THIS IS THE END OF THE GAME. PLEASE PLAY AGAIN.")
